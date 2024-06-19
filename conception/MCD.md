17240431656bbce7bc718dd11834affbaf53e158
ManuDiop <emmanuel.diop@gmail.com>
2024-06-19 18:07:17 +0200

    création de la section projects

---
+++ conception/MCD.md
@@ -0,0 +1,11 @@
+### On est sur un début d'idée à ce stade
+
+:
+Project: name, picture, description, link
+:
+
+apporte, 1N Project, 01 Experience
+
+Experience: techno name, utilisation
+
+
--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -6,12 +6,12 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class HomeController extends AbstractController
+class MainController extends AbstractController
 {
-    #[Route('/', name: 'app_home')]
+    #[Route('/', name: 'app_main')]
     public function index(): Response
     {
-        return $this->render('home/home.html.twig', [
+        return $this->render('main/main.html.twig', [
             'controller_name' => 'HomeController',
         ]);
     }
